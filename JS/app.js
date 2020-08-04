662285e449be1161d0191a5aa01c40c4ea779bdc
Raiden8051 <www.sidharth.56@gmail.com>
2020-08-04 07:50:25 +0530

    04/08/20

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+(function (){
+    angular.module('myFirstPage',[])
+    .controller('MyFirstController',function($scope){
+        $scope.name = "";
+        $scope.total = 0;
+        $scope.calculateSize = function () {
+            $scope.total = getTotal($scope.name); 
+        };
+        function getTotal(string)
+        {
+            var total = 0;
+            for(var i=0;i<string.length;i++){
+                total+=string.charCodeAt(i);
+            }
+            return total;
+        }
+    })
+    .controller('ToUpperController',makeUpper);
+
+    function makeUpper($scope, $filter) {
+        $scope.upper = function(){
+            var toupper = $filter('uppercase');
+            $scope.upperData = toupper($scope.upperData);
+    };
+}
+
+})();
