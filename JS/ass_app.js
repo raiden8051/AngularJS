6e137b648ce158225967db4f87684c82bdba1221
Raiden8051 <www.sidharth.56@gmail.com>
2020-08-04 15:58:34 +0530

    04/08/2020

---
+++ JS/ass_app.js
@@ -0,0 +1,42 @@
+(function () {
+    
+    angular.module('Assignment',[])
+    .controller('DataController',DataController);
+
+    DataController.$inject=['$scope'];
+
+    function DataController($scope){
+
+        $scope.data_items = "";
+        $scope.message = "";
+        $scope.color = "black";
+        $scope.checkInput = function () {
+            
+            var arr = $scope.data_items.split(",");
+
+            var ctr = 0;
+            for(var i=0;i<arr.length;i++)
+            {
+                if(arr[i].trim()!="")
+                    ctr++;
+            }
+            $scope.borderdata = "2px solid ";
+            if(ctr==0)
+            {
+                $scope.message = "Please Enter Data First!";
+                $scope.borderdata += "Crimson";
+                $scope.color = "Crimson";
+            }
+            else if(ctr>0 && ctr<3){
+                $scope.message = "Enjoy!";
+                $scope.borderdata += "LimeGreen";
+                $scope.color = "limegreen";
+            }
+            else{
+                $scope.message = "Too Much!";
+                $scope.borderdata += "Limegreen";
+                $scope.color = "limegreen";
+            }
+        };
+    }
+})();
